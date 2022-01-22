cef5e292fea930e42d3785aa524011014c639614
Oleg Klimov <omgtech@gmail.com>
2022-01-22 19:35:29 +0100

    Prepare for public release 1

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="smallcloud",
     py_modules=["smallcloud"],
-    version="0.0.1",
+    version="0.1.0",
+    url="https://github.com/smallcloudai/smallcloud",
+    license='GNU GPLv3',
     install_requires=[""],
+    author="Small Magellanic Cloud AI Ltd.",
+    author_email="cli-tool@smallcloud.tech",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Environment :: Console",
+        "Operating System :: OS Independent",
+    ]
 )
-
-# to install p7z run
-#
