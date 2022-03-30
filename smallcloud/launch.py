a67b2c11d3871877c751d4bd709ef791a7e58187
Oleg Klimov <omgtech@gmail.com>
2022-03-30 10:18:25 +0200

    launch works

--- smallcloud/launch.py
+++ smallcloud/launch.py
@@ -1,7 +1,7 @@
 import os, json, requests, termcolor
 from typing import Optional, List, Dict, Any, Union, Callable
 import cloudpickle
-from smallcloud import config
+from smallcloud import config, call_api
 
 
 MAX_UPLOAD_SIZE = 5*1024*1024
@@ -39,20 +39,32 @@ def code_upload(zip_fn: Optional[str] = None):
 
 
 def launch_task(
-    training_function: Union[Callable, str],
     task_name: str,
+    training_function: Union[Callable, str],
     args: List[Any] = [],
     kwargs: Dict[str, Any] = {},
     code_zip: Optional[str] = None,
-    gpu_type="a5000",
+    gpu_type="a5000",  # or more specifically cluster/gpu_type, "ant/a5000"
     gpus: int = 0,
-    # shutdown="auto",
-    # gpu_per_process: int = 1,
-    # gpu_bond: int = 1,
+    shutdown: str = "auto",  # "always", "never", "auto" doesn't shutdown if there's an error so you can look at the logs.
     # cluster: str = "default",
-    # priority: int = 1,  # 0 preempts others, 1 normal, 2 low
+    nice: int = 1,  # 0 preempts others, 1 normal, 2 low
+    os_image: str = "",
 ):
+    config.read_config_file()
     os.makedirs("/tmp/smc-temp", exist_ok=True)
     pickle_filename = f"/tmp/smc-temp/pickle-call-{task_name}.pkl"
     cloudpickle.dump(training_function, open(pickle_filename, "wb"))
     pickle_upload_id = upload_file(pickle_filename)
+    post_json = {
+    "task_name": task_name,
+    "tenant_image": os_image,
+    "gpu_type": gpu_type,
+    "gpu_min": int(gpus),
+    "gpu_max": int(gpus),
+    "gpus_incr": 1,
+    "file_zip": code_zip,
+    "file_pkl": pickle_upload_id,
+    "nice": nice,
+    }
+    call_api.fetch_json(config.v1_url + "reserve", post_json, headers=config.account_and_secret_key_headers())
